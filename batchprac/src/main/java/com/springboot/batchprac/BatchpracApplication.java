2ac02230ac6d032cbb865a37f072e5832c2ac869
J.K <luvrkdus@gmail.com>
2020-12-28 10:47:25 +0900

    spring 최소 설정

---
+++ batchprac/src/main/java/com/springboot/batchprac/BatchpracApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.batchprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchpracApplication.class, args);
+	}
+
+}
---
+++ batchprac/src/test/java/com/springboot/batchprac/BatchpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.batchprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchpracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
